35108f3ef956769f67b553c026685ff5b01813b2
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2021-06-21 15:08:08 +0200

    ADDED optuna learning rate finder

--- MyModel.py
+++ MyModel.py
@@ -1,44 +1,41 @@
-import os
 
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-from torchvision.datasets import MNIST
-from torchvision import transforms
 import pytorch_lightning as pl
 
 
 class MyModel(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, lr):
         super().__init__()
+        self.lr = lr
         self.model = nn.Sequential(
-            nn.Linear(32 * 32, 256),
+            nn.Linear(32 * 32 * 3, 1024),
             nn.ReLU(),
-            nn.Linear(256, 128),
+            nn.Linear(1024, 256),
             nn.ReLU(),
-            nn.Linear(128, 10),
+            nn.Linear(256, 10),
             nn.Softmax(dim=1))
 
     def forward(self, x):
-        x = x.view(x.size(1), -1)
+        x = x.view(x.size(0), -1)
         return self.model(x)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
         logits = self.forward(x)
-        loss = F.nll_loss(logits, y)
+        loss = F.cross_entropy(logits, y)
         self.log('train_loss', loss)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
         logits = self.forward(x)
-        loss = F.nll_loss(logits, y)
+        loss = F.cross_entropy(logits, y)
         self.log('val_loss', loss)
+        return loss
 
--- run.py
+++ run.py
@@ -1,10 +1,15 @@
+import optuna
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping
 from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import MNIST
 from torchvision.datasets import CIFAR10
 import os
 from torchvision import datasets, transforms
 from projects.classification.MyModel import MyModel
+from optuna.integration import PyTorchLightningPruningCallback
+from torch.nn import functional as F
+import numpy as np
 
 transform = transforms.Compose([transforms.ToTensor(),
                                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
@@ -17,10 +22,38 @@ cifar10_test = CIFAR10(os.getcwd(), train=False, download=True, transform=transf
 cifar10_train, cifar10_val = random_split(cifar10_train, [45000, 5000])
 
 #dataloaders
-cifar10_train = DataLoader(cifar10_train, batch_size=64)
-cifar10_val = DataLoader(cifar10_val, batch_size=64)
-cifar10_test = DataLoader(cifar10_test, batch_size=64)
+cifar10_train = DataLoader(cifar10_train, batch_size=1024)
+cifar10_val = DataLoader(cifar10_val, batch_size=1024)
+cifar10_test = DataLoader(cifar10_test, batch_size=1024)
 
-model = MyModel()
-trainer = Trainer(gpus=1)
-trainer.fit(model, train_dataloader=cifar10_train, val_dataloaders=cifar10_val)
+
+#optuna look for a good learning rate
+def objective(trial):
+    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+    model = MyModel(lr)
+    trainer = Trainer(gpus=1, max_epochs=10, callbacks=[EarlyStopping(monitor='val_loss')])
+    trainer.fit(model, train_dataloader=cifar10_train, val_dataloaders=cifar10_val)
+    losses = []
+    for batch in cifar10_val:
+        x, y = batch
+        logits = model.forward(x)
+        loss = F.cross_entropy(logits, y)
+        losses.append(loss.cpu().detach().numpy())
+    losses = np.asarray(losses)
+    loss = np.mean(losses)
+    return loss
+
+
+def run():
+    model = MyModel(1e-4)
+    trainer = Trainer(gpus=1, max_epochs=200, callbacks=[EarlyStopping(monitor='val_loss')])
+    trainer.fit(model, train_dataloader=cifar10_train, val_dataloaders=cifar10_val)
+
+
+#study = optuna.create_study()
+#study.optimize(objective, n_trials=100)
+
+#print(study.best_params)
+
+run()
+

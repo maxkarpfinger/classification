780af4cf4f94cdca75e3568e80bdd6b653e0b695
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2021-06-22 19:32:34 +0200

    UPDATED CNN architecture, ADDED jupyter notebook

--- run.py
+++ run.py
@@ -22,8 +22,8 @@ from projects.classification.MyCNN import MyCNN
 Path("./outputs").mkdir(parents=True, exist_ok=True)
 matplotlib.style.use('ggplot')
 
-transform = transforms.Compose([transforms.ToTensor()#,
-                               #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))]
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
                                 ])
 
 BATCH_SIZE = 1024
@@ -32,16 +32,16 @@ BATCH_SIZE = 1024
 cifar10_train = CIFAR10(os.getcwd(), train=True, download=True, transform=transform)
 cifar10_test = CIFAR10(os.getcwd(), train=False, download=True, transform=transform)
 
-#split train into train and validation
+# split train into train and validation
 cifar10_train, cifar10_val = random_split(cifar10_train, [45000, 5000])
 
-#dataloaders
+# dataloaders
 cifar10_train = DataLoader(cifar10_train, batch_size=BATCH_SIZE)
 cifar10_val = DataLoader(cifar10_val, batch_size=BATCH_SIZE)
 cifar10_test = DataLoader(cifar10_test, batch_size=BATCH_SIZE)
 
 
-#optuna look for a good learning rate
+# optuna look for a good learning rate
 def objective(trial):
     lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
     model = MyModel(lr)
@@ -57,7 +57,8 @@ def objective(trial):
     loss = np.mean(losses)
     return loss
 
-#fit the model
+
+# fit the model
 
 
 def run_linear():
@@ -68,16 +69,17 @@ def run_linear():
 
 
 def run_cnn():
-    model = MyCNN(1e-4)
+    model = MyCNN(1e-5)
     trainer = Trainer(gpus=1, max_epochs=200, callbacks=[EarlyStopping(monitor='val_loss', patience=5)])
     trainer.fit(model, train_dataloader=cifar10_train, val_dataloaders=cifar10_val)
     trainer.save_checkpoint("example.ckpt")
 
 
 def load_model():
-    return MyModel.load_from_checkpoint(checkpoint_path="D:/Uni/TUM/Master "
-                                                             "Informatik/WS20/projects/classification/lightning_logs"
-                                                             "/version_136/checkpoints/epoch=199-step=8799.ckpt", lr=1e-5)
+    return MyCNN.load_from_checkpoint(
+        checkpoint_path="D:/Uni/TUM/Master Informatik/WS20/projects/classification/lightning_logs/version_183"
+                        "/checkpoints/epoch=77-step=3431.ckpt",
+        lr=1e-4)
 
 
 def plot_predictions():
@@ -90,20 +92,21 @@ def plot_predictions():
     label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
     images = []
     fig = matplotlib.pyplot.figure()
-    for i in range(0, 9):
+    fig.set_size_inches(18.5, 10.5)
+    for i in range(0, 25):
         plt.axis('off')
         plt.imshow(first_batch[i].permute(1, 2, 0))
         if predictions[i] == labels[i]:
-            plt.title(label_names[predictions[i]], fontdict={'color': 'green', 'fontsize': 30})
+            plt.title(label_names[predictions[i]], fontdict={'color': 'green', 'fontsize': 60})
         else:
-            plt.title(label_names[predictions[i]], fontdict={'color': 'red', 'fontsize': 30})
+            plt.title(label_names[predictions[i]], fontdict={'color': 'red', 'fontsize': 60})
         plt.savefig(f"./outputs/{i}.png")
         img = mpimg.imread(f"./outputs/{i}.png")
         images.append(img)
         # plt.show()
     plt.title("Predictions", fontdict={'color': 'black', 'fontsize': 20})
     grid = ImageGrid(fig, 111,  # similar to subplot(111)
-                     nrows_ncols=(3, 3),  # creates 3x3 grid of axes
+                     nrows_ncols=(5, 5),  # creates 3x3 grid of axes
                      axes_pad=0.,  # pad between axes in inch.
                      )
     for ax, im in zip(grid, images):
@@ -116,12 +119,10 @@ def plot_predictions():
     plt.show()
 
 
+# study = optuna.create_study()
+# study.optimize(objective, n_trials=100)
+# print(study.best_params)
 
-#study = optuna.create_study()
-#study.optimize(objective, n_trials=100)
-#print(study.best_params)
+plot_predictions()
 
-#plot_predictions()
-
-run_cnn()
-
+#run_cnn()
